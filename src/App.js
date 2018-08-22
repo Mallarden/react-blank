610c7171fd8d75d8b1b0f58c0b3777a26ae44c77
Mallarden <antonanderson.com@gmail.com>
2018-08-22 10:49:03 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="text-center">En liten header</h1>
+        </header>
+        <div className="main">
+          <div className="container">
+          
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
